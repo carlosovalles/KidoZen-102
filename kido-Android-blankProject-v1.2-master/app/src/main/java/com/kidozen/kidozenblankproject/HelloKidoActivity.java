0adf62f498f7a84538cc4d497bf09a53ce594790
Carlos Ovalles <carlos.ovalles@kidozen.com>
2014-09-08 18:15:18 -0400

    added Storage Service

--- kido-Android-blankProject-v1.2-master/app/src/main/java/com/kidozen/kidozenblankproject/HelloKidoActivity.java
+++ kido-Android-blankProject-v1.2-master/app/src/main/java/com/kidozen/kidozenblankproject/HelloKidoActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import org.apache.http.HttpStatus;
+import org.json.JSONObject;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -18,47 +19,26 @@ import kidozen.client.InitializationException;
 import kidozen.client.KZApplication;
 import kidozen.client.ServiceEvent;
 import kidozen.client.ServiceEventListener;
+import kidozen.client.Storage;
 
 
 
 public class HelloKidoActivity extends Activity {
 
     public static final String TAG = "HelloKido Activity";
-    final String TENANT = "https://YOURMARKEPLACE.kidocloud.com/";
-    final String APPLICATION = "appName";
-    final String AppKey = "appKEY";
+    final String TENANT = "https://public.kidocloud.com/";
+    final String APPLICATION = "tasks";
+    final String AppKey = "vI672FJGweb0MG457fkNA8SDzxS08kOTOdQcHVon6+w=";
     final String KidoZenProvider = "Kidozen";
-    final String KidoZenUser = "USER@EMAIL.com";
-    final String KidoZenPassword = "?????";
+    final String KidoZenUser = "public@kidozen.com";
+    final String KidoZenPassword = "pass#1";
 
     Button authButton ;
     TextView textMessage;
 
     KZApplication app;
 
-    private View.OnClickListener authenticateWithKidozen = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
 
-            try {
-                app.Authenticate(KidoZenProvider, KidoZenUser, KidoZenPassword, new ServiceEventListener() {
-                    @Override
-                    public void onFinish(ServiceEvent e) {
-                        if (e.StatusCode != HttpStatus.SC_OK) {
-                            Log.d(TAG, "**** ERROR MESSAGE: Unable to reach the kidozen server. Make sure your KidoZenAppCenterUrl and KidoZenAppName are correct");
-                        } else {
-                            textMessage.setText("Hello: " + app.GetKidoZenUser().Claims.get("name"));
-                            Log.d(TAG, app.GetKidoZenUser().Claims.get("name"));
-                            Log.d(TAG, "KidoZen autentication sucessful.");
-                        }
-
-                    }
-                });
-            } catch (InitializationException e) {
-                e.printStackTrace();
-            }
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -87,7 +67,6 @@ public class HelloKidoActivity extends Activity {
         }
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -106,4 +85,67 @@ public class HelloKidoActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+    // HERE IS WHERE THE AUTHENTICATION HAPPENS
+    private View.OnClickListener authenticateWithKidozen = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            try {
+                app.Authenticate(KidoZenProvider, KidoZenUser, KidoZenPassword, new ServiceEventListener() {
+                    @Override
+                    public void onFinish(ServiceEvent e) {
+                        if (e.StatusCode != HttpStatus.SC_OK) {
+                            Log.d(TAG, "**** ERROR MESSAGE: Unable to reach the kidozen server. Make sure your KidoZenAppCenterUrl and KidoZenAppName are correct");
+                        } else {
+                            textMessage.setText("Hello: " + app.GetKidoZenUser().Claims.get("name"));
+                            Log.d(TAG, app.GetKidoZenUser().Claims.get("name"));
+                            Log.d(TAG, "KidoZen autentication sucessful.");
+
+                            //Launch Kidozen TestServices method
+                            LaunchKidozenServicesDemo();
+                        }
+
+                    }
+                });
+            } catch (InitializationException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+
+    private void LaunchKidozenServicesDemo(){
+
+        try {
+            CreateOrder(123124,"create order #124123");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+    public void CreateOrder(int id, String detail) throws  Exception{
+        Storage orders = app.Storage("order");
+        JSONObject order = new JSONObject().put("id", id).put("detail", detail);
+
+
+        orders.Create(order, new ServiceEventListener() {
+
+            JSONObject mMetadata;
+            @Override
+            public void onFinish(ServiceEvent e) {
+                if (e.StatusCode==HttpStatus.SC_CREATED) {
+                    // keep a reference of the response, which has the unique identifier of the message
+                    mMetadata = (JSONObject) e.Response;
+                    Log.d(TAG,mMetadata.toString());
+                }
+            }
+        });
+    }
+
+
 }
